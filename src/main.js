3c148e2f24c98bec0b70adaf41ca2555df89b027
taoka-toshiaki <71550806+taoka-toshiaki@users.noreply.github.com>
2023-03-21 15:20:47 +0900

    厳格化 *google Chrome拡張

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 chrome.tabs.query({ 'active': true,'lastFocusedWindow': true }, function (tabs) {
     var site = String(tabs[0].url).split("/");
-    if(site[4]){
+    if(site[4] && tabs[0].url.match(/https:\/\/github.com/)){
         document.getElementById("urls").href = "https://" + site[3] + ".github.io" + "/" + site[4];
         document.getElementById("urls").innerText = "https://" +  site[3] + ".github.io" + "/" + site[4];
     }
